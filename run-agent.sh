a3055de57c5c341c71a6e35faf7979449388a2c6
Torsten Werner <tw@to-wer.dev>
2024-06-04 13:51:15 +0200

    fixed run-agent.sh script

--- run-agent.sh
+++ run-agent.sh
@@ -2,7 +2,8 @@
 
 # Konfiguriere den TeamCity-Agenten
 if [ ! -f ./conf/buildAgent.properties ]; then
-  cp ./conf.dist/buildAgent.dist.properties ./conf/buildAgent.properties
+  # cp ./conf.dist/buildAgent.dist.properties ./conf/buildAgent.properties
+  cp ./conf/buildAgent.dist.properties ./conf/buildAgent.properties
   sed -i -e "s|^serverUrl=.*|serverUrl=${SERVER_URL}|" ./conf/buildAgent.properties
 fi
 
