96cb66b06f88b2cd316795dac15293b012b97272
Torsten Werner <tw@to-wer.dev>
2024-06-04 14:48:44 +0200

    test

--- run-agent.sh
+++ run-agent.sh
@@ -2,9 +2,10 @@
 
 # Konfiguriere den TeamCity-Agenten
 if [ ! -f ./conf/buildAgent.properties ]; then
-  # cp ./conf.dist/buildAgent.dist.properties ./conf/buildAgent.properties
-  cp ./conf/buildAgent.dist.properties ./conf/buildAgent.properties
-  cp ./conf/buildAgent.dist.properties /data/teamcity_agent/conf/buildAgent.properties
+  cp ./conf.dist/buildAgent.dist.properties ./conf/buildAgent.properties
+  cp ./conf.dist/buildAgent.dist.properties /data/teamcity_agent/conf/buildAgent.properties
+  # cp ./conf/buildAgent.dist.properties ./conf/buildAgent.properties
+  # cp ./conf/buildAgent.dist.properties /data/teamcity_agent/conf/buildAgent.properties
   sed -i -e "s|^serverUrl=.*|serverUrl=${SERVER_URL}|" /data/teamcity_agent/conf/buildAgent.properties
 fi
 
